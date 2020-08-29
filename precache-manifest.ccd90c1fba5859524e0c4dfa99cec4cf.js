336190692d4f1a5339176517b52b93574d664983
Subham95-git <roysubham505@gmail.com>
2020-08-29 11:30:16 +0530

    Updates

---
+++ precache-manifest.ccd90c1fba5859524e0c4dfa99cec4cf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c388506d73b96f986ba69e7b2b6392",
+    "url": "/Mobile_Cart/index.html"
+  },
+  {
+    "revision": "b9cc5a547fbee74d37c5",
+    "url": "/Mobile_Cart/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "91698cfb881dac81cd35",
+    "url": "/Mobile_Cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9cc5a547fbee74d37c5",
+    "url": "/Mobile_Cart/static/js/2.cb7bf22d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mobile_Cart/static/js/2.cb7bf22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91698cfb881dac81cd35",
+    "url": "/Mobile_Cart/static/js/main.b7b1323f.chunk.js"
+  },
+  {
+    "revision": "6f6b8933e91520f5007d",
+    "url": "/Mobile_Cart/static/js/runtime-main.7cba206c.js"
+  },
+  {
+    "revision": "0a7e2f9c87d822f0040be59c33577d06",
+    "url": "/Mobile_Cart/static/media/Galaxy-S10-Plus.0a7e2f9c.jpg"
+  },
+  {
+    "revision": "d77cd6666ced74bd8925ea0fd7af370f",
+    "url": "/Mobile_Cart/static/media/iphone_x.d77cd666.jpg"
+  },
+  {
+    "revision": "fd9a5d4402f98e9ec93a40dc5610c913",
+    "url": "/Mobile_Cart/static/media/onepluse.fd9a5d44.png"
+  },
+  {
+    "revision": "30914bf12c8a6638c0d6d5ff661268ba",
+    "url": "/Mobile_Cart/static/media/pixel_2_xl.30914bf1.png"
+  }
+]);
